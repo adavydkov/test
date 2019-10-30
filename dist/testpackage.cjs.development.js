2b41e3d4082ae37221bb64f9e848451ebc0b845f
a_davydkov <av.davydkov@gmail.com>
2019-10-30 11:08:17 +0300

    qqqqq11115555555

---
+++ dist/testpackage.cjs.development.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var React = require('react');
+
+var Thing = function Thing() {
+  return React.createElement("div", null, "the snozzberries taste like snozzberries");
+};
+
+exports.Thing = Thing;
+//# sourceMappingURL=testpackage.cjs.development.js.map
